b9c12f084b680b4631ef9b2ffdcb97ab9c32c08e
Roy_XiongYC <764340703@qq.com>
2020-07-21 00:06:40 +0800

    1

--- service-parent/service-weChat/src/main/java/com/xiongyc/product/controller/H5Controller.java
+++ service-parent/service-weChat/src/main/java/com/xiongyc/product/controller/H5Controller.java
@@ -3,6 +3,7 @@ package com.xiongyc.product.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -17,5 +18,10 @@ public class H5Controller {
 	public String queryPage() {
 		return "index";
 	}
-
+	
+	@ResponseBody
+	@GetMapping("/getToken")
+	public String getToken() { 
+		return "index";
+	}
 }
