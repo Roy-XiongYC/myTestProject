9003dc95f6a2536811cbf31d36856a921367ba46
xiongyc <764340703@qq.com>
2020-07-25 20:38:55 +0800

    1

--- service-parent/service-weChat/src/main/java/com/xiongyc/product/controller/H5Controller.java
+++ service-parent/service-weChat/src/main/java/com/xiongyc/product/controller/H5Controller.java
@@ -1,12 +1,15 @@
 package com.xiongyc.product.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -19,13 +22,14 @@ public class H5Controller {
 	@Autowired
 	private CoreController WxCore;
 
-	@GetMapping("/redirectUrl")
+	@GetMapping("/redirect")
 	public String redirectUrl(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
 			Model model) {
 		System.out.println(code);
 		System.out.println(state);
 		model.addAttribute("userInfo",WxCore.getAccessToken(code));
-		return "views/userInfo";
+//		return "views/userInfo";
+		return "redirect:http://47.107.87.255/admin/";
 	}
 
 	@ApiOperation(value = "翻页查询列表")
@@ -34,9 +38,13 @@ public class H5Controller {
 		return "index";
 	}
 
-	@ResponseBody
-	@GetMapping("/getToken")
-	public String getToken() {
-		return "index";
+//	@GetMapping("/redirect")
+//	public String getToken() {
+//		 return "redirect:http://47.107.87.255/admin/";
+//	}
+	
+	@GetMapping("/redirect1")
+	public void getToken(HttpServletResponse response) throws IOException {
+		 response.sendRedirect("http://47.107.87.255/admin/");
 	}
 }
