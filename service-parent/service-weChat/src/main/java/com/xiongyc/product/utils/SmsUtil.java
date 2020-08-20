cfe831c76da5d05f6dc0786f21de29c6bfac11d0
xiongyc <764340703@qq.com>
2020-08-20 11:53:55 +0800

    1

--- service-parent/service-weChat/src/main/java/com/xiongyc/product/utils/SmsUtil.java
+++ service-parent/service-weChat/src/main/java/com/xiongyc/product/utils/SmsUtil.java
@@ -15,31 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SmsUtil {
 
-//	public static void main(String[] args) {
-//		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4G9WTFC3xKc5b7YnvHc6", "YEjtBzZAyWaHK6bS24h1aJluQ3jnpp");
-//		IAcsClient client = new DefaultAcsClient(profile);
-//
-//		CommonRequest request = new CommonRequest();
-//		request.setSysMethod(MethodType.POST);
-//		request.setSysDomain("dysmsapi.aliyuncs.com");
-//		request.setSysVersion("2017-05-25");
-//		request.setSysAction("SendSms");
-//		request.putQueryParameter("RegionId", "cn-hangzhou");
-//		request.putQueryParameter("PhoneNumbers", "13732924865");
-//		request.putQueryParameter("SignName", "迪卡宠物生活馆01");
-//		request.putQueryParameter("TemplateCode", "SMS_199797345");
-//		request.putQueryParameter("TemplateParam", "{\"code\":\"DD2020202025415\"}");
-//		request.putQueryParameter("OutId", "131231231");
-//		try {
-//			CommonResponse response = client.getCommonResponse(request);
-//			System.out.println(response.getData());
-//		} catch (ServerException e) {
-//			e.printStackTrace();
-//		} catch (ClientException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
 	public static void send(String regionId, String accessKeyId, String secret,String domain, String version, String action,String mobile, String signName, String templateCode, String templateParamJson, String outId) {
 		DefaultProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, secret);
 		IAcsClient client = new DefaultAcsClient(profile);
